2af0f5c110bf5fa3a62e42a2e6e56b5f7d9e9a49
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-01-03 15:17:05 -0300

    product tests

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -2,16 +2,24 @@ describe('product tests', () => {
 
   beforeEach(() => {
     cy.visit(Cypress.env("url"))
+    cy.xpath('//a[@title="Women"]').as('filterWomenButton')
   })
 
-  it('search product', () => {
+  it('search a product', () => {
     cy.get('#search_query_top').type('dress')
     cy.get('.button-search').click()
     cy.get('.lighter').contains('dress')
   })
 
-  it.only('filter product', () => {
-    cy.xpath('//a[@title="Women"]').click()
+  it('filter a product', () => {
+    cy.get('@filterWomenButton').click()
+    cy.get('.category-name').contains('Women').should('be.visible')
+  })
+
+  it.only('click in a product', () => {
+    cy.get('@filterWomenButton').click()
+    cy.get('.product-container .product-name').first().click()
+    cy.get('#buy_block').should('be.visible')
   })
 
 })
