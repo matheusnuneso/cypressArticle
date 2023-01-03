dad5d95b6500e5a6dd6a3730e27ccde21d90b162
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-01-03 15:28:49 -0300

    add locators file

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,8 +1,9 @@
+import locators from "../support/locators"
+
 describe('product tests', () => {
 
   beforeEach(() => {
     cy.visit(Cypress.env("url"))
-    cy.xpath('//a[@title="Women"]').as('filterWomenButton')
   })
 
   it('search a product', () => {
@@ -12,12 +13,12 @@ describe('product tests', () => {
   })
 
   it('filter a product', () => {
-    cy.get('@filterWomenButton').click()
+    cy.xpath(locators.xpButtonWomenFilter).click()
     cy.get('.category-name').contains('Women').should('be.visible')
   })
 
-  it.only('click in a product', () => {
-    cy.get('@filterWomenButton').click()
+  it('click in a product', () => {
+    cy.xpath(locators.xpButtonWomenFilter).click()
     cy.get('.product-container .product-name').first().click()
     cy.get('#buy_block').should('be.visible')
   })
---
+++ cypress/support/locators.js
@@ -0,0 +1,5 @@
+const locators = {
+    xpButtonWomenFilter: '//a[@title="Women"]'
+}
+
+export default locators;
